ae6e5c731eb8f6db3bbfd878b41258b404f9624a
fedsalluzz <fed.salluzzo@gmail.com>
2020-06-28 19:32:12 +0100

    Add support for multiple ticks

--- finance.py
+++ finance.py
@@ -5,14 +5,11 @@
 import platform
 import os
 import time
-
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.rcParams['axes.grid'] = True
-
 import numpy as np
-import os
 from tkinter import *
 import requests
 
@@ -24,8 +21,12 @@ print('Your pithon version is: ' + platform.python_version())
 #r = requests.get(Financial_api_endpoint)
 #print(r.text)
 
-
+#
+# Retrieve data from API endpoint
+#
+#de
 def Get_data_from_api(document,symbol,period,api_key):
+	print('\n---- GET DATA FROM API starting...')
 	api_endpoint_head ='https://financialmodelingprep.com/api/v3/'
 	if (document == 'income_statement'):
 		api_endpoint_body = api_endpoint_head+'income-statement/'
@@ -43,36 +44,54 @@ def Get_data_from_api(document,symbol,period,api_key):
 		api_endpoint =api_endpoint_body+symbol+'?period=quarter&apikey='+api_key
 	else:
 		api_endpoint =api_endpoint_body+symbol+'?apikey='+api_key
-	r = requests.get(api_endpoint)
-	print('-- API ENDPOINT IS : '+api_endpoint)
-	print('-- GET '+document+' from endpoint' )
+	try:
+		r = requests.get(api_endpoint)
+		print('-- API ENDPOINT IS : '+api_endpoint)
+		print('-- GET '+document+' from endpoint' )
+	except:
+		print('**** EXCEPTION in Get_data_from_api :: message from API endopoint: '+r.text)
 	return r.text
 
-
-def Plot_statistic(company,title_data, x_data, y1_data, y2_data, y3_data = None,y4_data = None):
-	print('-- Trying to plot statistics ')
-	if (len(title_data) == 2):
-		fig, axs = plt.subplots(nrows=2,ncols=1, num=company+' retrieved data')
-		axs[0].plot(x_data,y1_data, 'tab:green')
-		axs[0].set_title(str(title_data[0]))
-		axs[1].plot(x_data,y2_data, 'tab:blue')
-		axs[1].set_title(str(title_data[1]))
+#
+# Plot wanted statitics for 2x1 or 2x2 suplots
+#
+#def Plot_statistic(company,title_data, x_data, y1_data, y2_data, y3_data = None,y4_data = None):
+def Plot_statistic(ticker_list,title_data,tickers_dict, tickers_dict_2 = None ):
+	print('\n---- Trying to plot statistics ')
+	if (len(ticker_list) == 1):
+		print('--Plotting statics for: '+str(ticker_list))
+		if (len(title_data) == 2):
+			fig, axs = plt.subplots(nrows=2,ncols=1, num=str(ticker_list)+' retrieved data')
+			for ticker in ticker_list:
+				#print(str(ticker))
+				#print(tickers_dict)
+				
+				axs[0].plot(tickers_dict[str(ticker)]['x'],tickers_dict[str(ticker)]['y1'], 'tab:green',label = ticker)
+				axs[0].set_title(str(title_data[0]))
+				axs[1].plot(tickers_dict[str(ticker)]['x'],tickers_dict[str(ticker)]['y2'], 'tab:blue', label = ticker)
+				axs[1].set_title(str(title_data[1]))
+		else:
+			ticker = ticker_list[0]
+			fig, axs = plt.subplots(nrows=2,ncols=2, num=str(ticker)+' retrieved data')
+			axs[0,0].plot(tickers_dict[str(ticker)]['x'],tickers_dict[str(ticker)]['y1'], 'tab:green')
+			axs[0,0].set_title(str(title_data[0]))
+			axs[0,1].plot(tickers_dict[str(ticker)]['x'],tickers_dict[str(ticker)]['y2'], 'tab:blue')
+			axs[0,1].set_title(str(title_data[1]))
+			axs[1,0].plot(tickers_dict_2[str(ticker)]['x'],tickers_dict_2[str(ticker)]['y1'], 'tab:red')
+			axs[1,0].set_title(str(title_data[2]))
+			axs[1,1].plot(tickers_dict_2[str(ticker)]['x'],tickers_dict_2[str(ticker)]['y2'], 'tab:orange')
+			axs[1,1].set_title(str(title_data[3]))
 	else:
-		fig, axs = plt.subplots(nrows=2,ncols=2, num=company+' retrieved data')
-		axs[0,0].plot(x_data,y1_data, 'tab:green')
-		axs[0,0].set_title(str(title_data[0]))
-		axs[0,1].plot(x_data,y2_data, 'tab:blue')
-		axs[0,1].set_title(str(title_data[1]))
-		axs[1,0].plot(x_data,y3_data, 'tab:red')
-		axs[1,0].set_title(str(title_data[2]))
-		axs[1,1].plot(x_data,y4_data, 'tab:orange')
-		axs[1,1].set_title(str(title_data[3]))
+		print('--Plotting statics for: '+str(ticker_list))
+		pass
 	fig.autofmt_xdate()
 	print(' -- --- Plotting the graphs over time')
 	plt.show()
 	return #plt.show() 
 
-
+#
+# Sort data from oldest to newest
+#
 def Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax):
 	if (years == "MAX"):
 		pass
@@ -87,33 +106,52 @@ def Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax):
 	print('-- Selected data for '+years+' years')
 	return (x_ax_order,y1_ax_order,y2_ax_order)
 
+#
+# Create dataframe with EPS data from income_statement (x,y1 and y2 axis)
+#
 def EPS(ticker,years, financial_api_key):
+	print('\n---- Starting the EPS function...')
 	eps_data = Get_data_from_api('income_statement',ticker,'quarter', financial_api_key) #it is a string json format
 	#print(eps_data)
 	load = json.loads(eps_data) #list
-	df = pd.DataFrame(load)
-	print('-- Income Statement Loaded , Dataframe loaded, Selecting EPS and EPS diluted')
-	x_ax = df['date'].to_numpy()
-	y1_ax = df['eps'].to_numpy()
-	y2_ax = df['epsdiluted'].to_numpy()
-	x_ax_order,y1_ax_order,y2_ax_order = Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax)
-	title_data = ['EPS','EPS diluted']
-	#Plot_statistic(ticker,title_data,x_ax_order, y1_ax_order, y2_ax_order)
-	return title_data , x_ax_order, y1_ax_order, y2_ax_order  
+	try:
+		df = pd.DataFrame(load)
+		print('-- Income Statement Loaded , Dataframe loaded, Selecting EPS and EPS diluted')
+		x_ax = df['date'].to_numpy()
+		y1_ax = df['eps'].to_numpy()
+		y2_ax = df['epsdiluted'].to_numpy()
+		x_ax_order,y1_ax_order,y2_ax_order = Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax)
+		title_data = ['EPS','EPS diluted']
+		print(x_ax_order)
+		print('--- Closing the EPS function...')
+	except:
+		print('**** EXCEPTION in ESP function :: message from API endopoint: '+eps_data)
+		exit
+	return title_data , x_ax_order.tolist(), y1_ax_order.tolist(), y2_ax_order.tolist()  
 
+#
+# Create dataframe with book vlue and debt per earning data from metrics (x,y1 and y2 axis)
+#
 def BV_DEBT(ticker,years, financial_api_key):
+	print('\n---- Starting the BV_DEBT function...')
 	bv_data = Get_data_from_api('metrics',ticker,'quarter', financial_api_key) #it is a string json format
 	load = json.loads(bv_data) #list
-	df = pd.DataFrame(load)
-	#print(bv_data)
-	print('-- Income Statement Loaded , Dataframe loaded, Selecting BV and Debt')
-	x_ax = df['date'].to_numpy()
-	y1_ax = df['bookValuePerShare'].to_numpy()
-	y2_ax = df['debtToEquity'].to_numpy()
-	x_ax_order,y1_ax_order,y2_ax_order = Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax)
-	title_data = ['bookValuePerShare','debtToEquity']
-	#Plot_statistic(ticker,title_data,x_ax_order, y1_ax_order, y2_ax_order)
-	return  title_data , x_ax_order, y1_ax_order, y2_ax_order
+	try:
+		df = pd.DataFrame(load)
+		#print(bv_data)
+		print('-- Income Statement Loaded , Dataframe loaded, Selecting BV and Debt')
+		x_ax = df['date'].to_numpy()
+		y1_ax = df['bookValuePerShare'].to_numpy()
+		y2_ax = df['debtToEquity'].to_numpy()
+		x_ax_order,y1_ax_order,y2_ax_order = Time_and_statistic_sorter(years,x_ax,y1_ax,y2_ax)
+		title_data = ['bookValuePerShare','debtToEquity']
+		print(x_ax_order)
+		print('--- Closing the BD_DEBT function...')
+	except:
+		print('**** EXCEPTION in BV_DEBT function :: message from API endopoint: '+eps_data)
+		exit
+
+	return  title_data , x_ax_order.tolist(), y1_ax_order.tolist(), y2_ax_order.tolist()
 
 
 def CURRENT_RATIO(ticker, financial_api_key):
@@ -121,10 +159,32 @@ def CURRENT_RATIO(ticker, financial_api_key):
 	return current_ratio_data
 
 
-
+#
+# Check if the wanted data are present into internal database. Avoid max count to be reached fro API request
+#in trial version it is up to 200 calls
+#
+#def Ticker_data_is_saved(ticker,document):
+#	print('----\n Checking if '+ticker+' data has been previously saved...')
+#	file = open("user_requests.log","r")
+#	content = file.read()
+#	dictionary = ast.litteral_eval(content)
+#	for tick in dictionary:
+#		if (tick == ticker);
+#			print('-- Some data found...')
+#			if(dictionary[tick][document] == True )
+#				print('-- '+document+' data has been found in previous research. New data will not be request')
+#				found = True
+#			else:
+#				print('-- '+document+' data has *NOT* been found in previous research. New data will be request')
+#
+#				found = False
+#	return found 
+#
+#
+#
+##################################################
+##### MAIN #######################################
 #################################################
-#### MAIN #######################################
-################################################
 
 financial_api_key = 'c69c0e1c9f3b70a951441dc376b6d400'
 #document = 'metrics'
@@ -141,34 +201,85 @@ financial_api_key = 'c69c0e1c9f3b70a951441dc376b6d400'
 ###############################################
 
 def ESP_click():
-	entered_ticker = textTicker.get()
+	tickers_dict = dict()
+	entered_tickers=textTicker.get()
 	entered_years = buttonYears_var.get()
-	print('-- Selected ticker is/are: '+entered_ticker)
-	(title_data,x_ax_order,y1_ax_order,y2_ax_order) = EPS(entered_ticker,entered_years, financial_api_key)
-	Plot_statistic(entered_ticker,title_data,x_ax_order, y1_ax_order, y2_ax_order)
+	print('\n---- CLICK EVENT -- Selected ticker is/are: '+entered_tickers)
+	ticker_list = entered_tickers.split(',')
+	if (len(ticker_list) > 1):
+		print('-- You selected '+str(len(ticker_list))+' tickers\n')
+		for ticker in ticker_list:
+			print('---- Collecting data for '+ticker)
+			(title_data,x_ax_order,y1_ax_order,y2_ax_order) = EPS(ticker,entered_years, financial_api_key)
+			tickers_dict[ticker] = { 'x' : x_ax_order , 'y1' : y1_ax_order, 'y2' : y2_ax_order}
+			Plot_statistic(ticker_list,title_data,tickers_dict)
+	else:
+		print('-- You selected '+str(len(ticker_list))+' tickers\n')
+		print('---- Collecting data for '+ticker_list[0])
+		(title_data,x_ax_order,y1_ax_order,y2_ax_order) = EPS(entered_tickers,entered_years, financial_api_key)
+		print(str(title_data))
+		print(x_ax_order)
+		tickers_dict[ticker_list[0]] = { 'x' : x_ax_order , 'y1' : y1_ax_order, 'y2' : y2_ax_order}
+		print(tickers_dict)
+		Plot_statistic(ticker_list,title_data,tickers_dict)
 	return
 
 def BV_DEBT_click():
-	entered_ticker=textTicker.get()
-	entered_years = buttonYears_var.get() 
-	print('-- Selected ticker is: '+entered_ticker)
-	(title_data,x_ax_order,y1_ax_order,y2_ax_order) = BV_DEBT(entered_ticker,entered_years, financial_api_key)
-	Plot_statistic(entered_ticker,title_data,x_ax_order, y1_ax_order, y2_ax_order)
+	tickers_dict = dict()
+	entered_tickers=textTicker.get()
+	entered_years = buttonYears_var.get()
+	print('-- Selected ticker is/are: '+entered_tickers)
+	ticker_list = entered_tickers.split(',')
+	if (len(ticker_list) > 1):
+		print('-- You selected '+str(len(ticker_list))+' tickers \n')
+		for ticker in ticker_list:
+			print('---- Collecting data for '+str(ticker))
+			(title_data,x_ax_order,y1_ax_order,y2_ax_order) = BV_DEBT(ticker,entered_years, financial_api_key)
+			tickers_dict[ticker] = { 'x' : x_ax_order , 'y1' : y1_ax_order, 'y2' : y2_ax_order} 
+	else:
+		print('---- Collecting data for '+ticker_list[0])
+		(title_data,x_ax_order,y1_ax_order,y2_ax_order) = BV_DEBT(entered_tickers,entered_years, financial_api_key)
+		print(str(title_data))
+		print(x_ax_order)
+		tickers_dict[ticker_list[0]] = { 'x' : x_ax_order , 'y1' : y1_ax_order, 'y2' : y2_ax_order}
+		print(tickers_dict)
+		Plot_statistic(ticker_list,title_data,tickers_dict)
 	return
 
 def ANALYZE_click():
-	entered_ticker=textTicker.get()
+	tickers_dict_eps = dict()
+	tickers_dict_debt = dict()
+	entered_tickers=textTicker.get()
 	entered_years = buttonYears_var.get() 
-	print('-- Selected ticker is: '+entered_ticker)
-	(eps_title,x_eps,y1_eps,y2_eps) = EPS(entered_ticker,entered_years, financial_api_key) #data are ordered here
-	(debt_title,x_debt,y1_debt,y2_debt) = BV_DEBT(entered_ticker,entered_years, financial_api_key) #data are ordered
-	title_data = eps_title+debt_title
-	if (len(x_eps) == len(x_debt)):
-		x_ax_order = x_eps
-		Plot_statistic(entered_ticker,title_data,x_ax_order,y1_eps,y2_eps,y1_debt,y2_debt)
+	print('-- Selected ticker is/are: '+entered_tickers)
+	ticker_list = entered_tickers.split(',')
+	if (len(ticker_list) > 1):
+		print('-- You selected '+str(len(ticker_list))+' tickers \n')
+		print('---- Collecting EPS and BD and DEBT data for '+ticker_list[0])
+		for ticker in ticker_list:
+			print('---- Collecting data for '+str(ticker))
+			(eps_title,x_eps,y1_eps,y2_eps) = EPS(ticker,entered_years, financial_api_key) 
+			(debt_title,x_debt,y1_debt,y2_debt) = BV_DEBT(ticker,entered_years, financial_api_key) 
+			tickers_dict_eps[ticker] = { 'x' : x_eps , 'y1' : y1_eps, 'y2' : y2_eps} 
+			tickers_dict_debt[ticker] = { 'x' : x_debt , 'y1' : y1_debt, 'y2' : y2_debt}
 	else:
-		print('**** ERROR time lists not equal lenght ********')
-		exit
+		
+		print('-- You selected '+str(len(ticker_list))+' tickers \n')
+		print('---- Collecting EPS and BD and DEBT data for '+ticker_list[0])
+		(eps_title,x_eps,y1_eps,y2_eps) = EPS(entered_tickers,entered_years, financial_api_key) 
+		(debt_title,x_debt,y1_debt,y2_debt) = BV_DEBT(entered_tickers,entered_years, financial_api_key) 
+		tickers_dict_eps[ticker_list[0]] = { 'x' : x_eps , 'y1' : y1_eps, 'y2' : y2_eps} 
+		tickers_dict_debt[ticker_list[0]] = { 'x' : x_debt , 'y1' : y1_debt, 'y2' : y2_debt}
+		print(str(eps_title+debt_title))
+		title_data = eps_title+debt_title
+		Plot_statistic(ticker_list,title_data,tickers_dict_eps,tickers_dict_debt)
+		if (len(x_eps) == len(x_debt)):
+			x_ax_order = x_eps
+			Plot_statistic(ticker_list,title_data,tickers_dict_eps,tickers_dict_debt)
+			print(tickers_dict_eps)
+		else:
+			print('**** ERROR time lists not equal lenght ********')
+			exit
 	return
 
 
